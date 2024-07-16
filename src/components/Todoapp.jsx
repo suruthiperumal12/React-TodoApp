c4ba15e637af843233214e4e36b3565400d661f3
suruthiperumal12 <suruthi12jan@gmail.com>
2024-07-16 20:33:39 +0530

    modified files

--- src/components/Todoapp.jsx
+++ src/components/Todoapp.jsx
@@ -4,7 +4,15 @@ import Todolist from "./Todolist";
 import Footer from "./Footer";
 
 export default function Todoapp() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  // Use useEffect to update local storage whenever the todos state changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const completedtodos = todos.filter((todo) => todo.done).length;
   const totalcount = todos.length;
   return (
